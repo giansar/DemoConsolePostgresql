9996737e61a491a7cd7844d61d85cd6309dc4c34
Giansar Pratiknya <gpratiknya@gmail.com>
2022-03-19 17:52:23 +0700

    first commit

---
+++ DemoConsolePostgresql/Program.cs
@@ -0,0 +1,23 @@
+﻿using Npgsql;
+
+const string connectionString = "Host=localhost;Username=postgres;Password=yourpassword;Database=postgres";
+
+await using var connection = new NpgsqlConnection(connectionString);
+await connection.OpenAsync();
+
+await using (var command = new NpgsqlCommand("insert into table_test (column_one) values (@column_one)", connection))
+{
+    command.Parameters.AddWithValue("column_one", "GREEN LEAFS");
+    await command.ExecuteNonQueryAsync();
+}
+
+await using (var command = new NpgsqlCommand("select column_one from table_test", connection))
+{
+    await using (var reader = await command.ExecuteReaderAsync())
+    {
+        while (await reader.ReadAsync())
+        {
+            Console.WriteLine(reader.GetString(0));
+        }
+    }
+}
